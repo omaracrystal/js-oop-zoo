a58327ae84adfca5dc6bc185a59bff0868993efd
omaracrystal <omaracrystal@gmail.com>
2015-08-05 15:04:19 -0600

    added methods to animal prototype

--- src/animal.js
+++ src/animal.js
@@ -2,13 +2,46 @@ function Animal(name, age, kind){
   this.name = name;
   this.age = age;
   this.kind = kind;
-}
+  this.awake = false;
+};
 
+
+//oink() - returns "oink!" if the instance of an animal is a pig.
 Animal.prototype.oink = function(){
   if (this.kind === 'Pig'){
     return 'oink';
   } else {
     return "I'm not a pig";
+  }
+};
+
+
+//growUp() - a method that increases the age of the animal by one
+Animal.prototype.growUp = function () {
+  return (age + 1);
+};
+
+
+//feed() - if the animal is awake, return "NOM NOM NOM"
+Animal.prototype.feed = function () {
+  if (this.awake === true) {
+    return "NOM NOM NOM"
+  }
+};
+
+
+//wakeUp() - a method that changes the awake property to true
+Animal.prototype.wakeUp = function(){
+  if (this.awake === false) {
+    return (this.awake = true);
+  }
+};
+
+
+//sleep() - a method that changes the awake property to false
+Animal.prototype.sleep = function(argument){
+  if (this.awake === true) {
+    return (this.awake === false);
   }
 };
 
